daa3b6b1d6480a9c5da3f61e117f4dea8431cb42
laetitia fortineau <laetitia.fortineau@gmail.com>
2023-02-07 09:47:41 +0100

    terminé, voir pour rendre plus visible le nom,capital et pop. pays

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import Card from "./Card";
 
 const Countries = () => {
   const [data, setData] = useState([]);
+  const [rangeValue, setrangeValue] = useState(36);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
@@ -12,11 +16,41 @@ const Countries = () => {
 
   return (
     <div className="countries">
-      <h1>Pays</h1>
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setrangeValue(e.target.value)}
+        />
+        {radios.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+              checked={continent === selectedRadio}
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
+      </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          Annuler la recherche
+        </button>
+      )}
       <ul>
-        {data.map((country, index) => (
-          <li key={index}>{country.translations.fra.common}</li>
-        ))}{" "}
+        {data
+
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            <Card key={index} country={country} />
+          ))}{" "}
       </ul>
     </div>
   );
